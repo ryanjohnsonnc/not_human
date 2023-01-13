cc3e1ad99bd09b184309e70153e2ab7b4e643923
Ryan Johnson <ryanjohnson1989@me.com>
2023-01-12 21:15:46 -0500

    feat: basic gpt call

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import openai
 
 def askGPT(text):
-  openai.key = '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'
-  response = openai.Completion.create(
-      engine = "text-davinci-003",
-      prompt = text,
-      temperature = 0.6,
-      max_tokens = 150
-  )
-  return print(response.choices[0].text)
+    openai.api_key = "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"
+    response = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = text,
+        temperature = 0.6,
+        max_tokens = 150,
+    )
+    return print(response.choices[0].text)
+
+def main():
+    while True:
+        print('GPT: Ask me a question\n')
+        myQn = input()
+        askGPT(myQn)
+        print('\n')
+
+main()
