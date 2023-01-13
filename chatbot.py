ef86e0871516a9629a87cbdaf33b2117e0ab53c3
Ryan Johnson <ryanjohnson1989@me.com>
2023-01-12 22:08:18 -0500

    feat: function to build chat sequentially

--- chatbot.py
+++ chatbot.py
@@ -20,3 +20,9 @@ def ask(question, chat_log=None):
         max_tokens=150)
     answer = response.choices[0].text.strip()
     return answer
+  
+
+def append_interaction_to_chat_log(question, answer, chat_log=None):
+    if chat_log is None:
+        chat_log = start_chat_log
+    return f'{chat_log}Human: {question}\nAI: {answer}\n'
