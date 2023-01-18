e8b05a06b210f6e722cca3e2af95e365e219ca81
ryanjohnsonnc <ryanjohnson1989@me.com>
2023-01-17 21:35:31 -0500

    feat: implement chatbot, save chat log to session, append new, messages to log

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, session
 from twilio.twiml.messaging_response import MessagingResponse
+from chatbot import ask, append_interaction_to_chat_log
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'scwhing'
 
 
 @app.route('/bot', methods=['POST'])
 def bot():
     incoming_msg = request.values['Body']
+    chat_log = session.get('chat_log')
 
-    # use the incoming message to generate the response here
+    answer = ask(incoming_msg, chat_log)
+    session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
+                                                         chat_log)
 
     r = MessagingResponse()
-    r.message('this is the response')
+    r.message(answer)
     return str(r)
